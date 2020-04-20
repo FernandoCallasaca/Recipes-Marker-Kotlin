f60407f70b69416a2c356563585a61da7b59bf4e
Fernando Callasaca <fercallasaca@gmail.com>
2020-04-20 13:01:28 -0500

    Hicimos el cambio de modularidad para makeRecipe y viewRecipe

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -4,6 +4,25 @@ fun Menu(){
     println("2. Ver mis recetas")
     println("3. Salir")
 }
+fun makeRecipe(){
+    println("""
+        Hacer receta
+        Selecciona por categoría el ingrediente que buscas
+        1. Agua
+        2. Leche
+        3. Carne
+        4. Verduras
+        5. Frutas
+        6. Cereal
+        7. Huevos
+        8. Aceites
+    """.trimIndent())
+}
+fun viewRecipe(){
+    println("""
+        Ver mis recetas
+    """.trimIndent())
+}
 fun main(args: Array<String>) {
     println("""
         :: Bienvenido a Recipe Maker ::
@@ -14,8 +33,8 @@ fun main(args: Array<String>) {
         Menu()
         opcion = readLine()
         when(opcion.toString()){
-            in "1" -> println("Hacer una receta")
-            in "2" -> println("Ver mis recetas")
+            in "1" -> makeRecipe()
+            in "2" -> viewRecipe()
             else -> println("Ingrese una opcion correcta")
         }
 
